79b9e470383ff6adaa68884825c0bd141add52ea
Gaurav Baruah <GauravKBaruah@users.noreply.github.com>
2024-08-29 15:05:45 +0200

    Update figS30_S31.R

--- appendix_data/figS30_S31.R
+++ appendix_data/figS30_S31.R
@@ -53,21 +53,6 @@ n2<-(net_dat %>%
                    aes(color=factor(growth_rate))) +
        facet_grid(individual_variation~mut_strength))
 
-n3<-(net_dat %>% filter(network_size < 160) %>% 
-       ggplot(aes(x=network_size, y = recovery_richness, color= factor(handling_time)))+
-       geom_point(position=position_jitter(height=0.0,width=0.0),
-                  alpha = 0.05, size = 3)+
-       scale_color_viridis(discrete = TRUE)+
-       ylab("Recovery richness")+
-       labs(color=expression("handling time"))+
-       # theme(legend.position="none")+
-       xlab("Network size")+
-       # xlab(expression(paste(gamma[0],"," ,"mutualism strength")))+
-       stat_smooth(method = "glm", size=2,alpha=0.1,
-                   method.args = list(family = "quasibinomial"),
-                   se =FALSE,
-                   aes(color=factor(handling_time))) +
-       facet_grid(individual_variation~mut_strength))
 
 
 
